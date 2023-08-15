b06aa97fe6bd712818030b0ee2a5ce53bdf11f43
Julian Uy <uyjulian@gmail.com>
2023-08-15 15:39:34 +0000

    Use win32 lean and mean

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
 
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
+#include <objidl.h>
 #endif
 #include "tp_stub.h"
 
--- tjsCommHead.h
+++ tjsCommHead.h
@@ -2,7 +2,11 @@
 #define __TJSCOMMHEAD_H_DEFINED
 
 #ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
+#include <objidl.h>
 #endif
 #include "tp_stub.h"
 
